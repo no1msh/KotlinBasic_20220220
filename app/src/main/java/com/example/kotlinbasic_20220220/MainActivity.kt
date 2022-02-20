dbf707a27daedfc10823ba8309b474247ae7d245
TFQ_Moon <no1msh1217@gmail.com>
2022-02-20 15:56:58 +0900

    나이 계산 > 토스트로 출력

--- app/src/main/java/com/example/kotlinbasic_20220220/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasic_20220220/MainActivity.kt
@@ -105,6 +105,11 @@ class MainActivity : AppCompatActivity() {
             val inputBirthYear = edtBirthYear.text.toString()
 
             Log.d("입력된 년도", inputBirthYear)
+
+//            2022 년의 나이? 출생년도 "2010" 처럼, String > 숫자로 바꿔야 계산 가능
+            val age = 2022 - inputBirthYear.toInt() + 1
+
+            Toast.makeText(this, "${age}살 입니다.", Toast.LENGTH_SHORT).show()
         }
     }
 }
